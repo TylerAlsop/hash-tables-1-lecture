9097cdf0e873ff855da26a7a0c37cb36c885bbe1
Tyler Alsop <tylerjalsop@gmail.com>
2020-09-02 09:32:59 -0600

    Forgot to save changes to this file before committing and pushing.

--- hash-new.py
+++ hash-new.py
@@ -1,24 +1,16 @@
+
 class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
-
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
-    def __repr__(self):
-        return f"HTEntry: ({self.key}, {self.value}) -> {self.next}"
-
-    def __str__(self):
-        return f"HTEntry: ({self.key}, {self.value}) -> {self.next}"
-
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
-MAX_32_BITS = 0xFFFFFFFF
-MAX_64_BITS = 0xFFFFFFFFFFFFFFFF
 
 
 class HashTable:
@@ -29,9 +21,11 @@ class HashTable:
     """
 
     def __init__(self, capacity):
-        self.capacity = capacity if capacity >= MIN_CAPACITY else MIN_CAPACITY
-        self.size = 0
-        self.storage = [None] * self.capacity
+        if capacity < MIN_CAPACITY:
+            capacity = MIN_CAPACITY
+            
+        self.capacity = capacity
+        self.table = [None] * capacity
 
     def get_num_slots(self):
         """
@@ -43,12 +37,14 @@ class HashTable:
         """
         return self.capacity
 
+
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
         Implement this.
         """
-        return self.size / self.capacity
+        # Your code here
+
 
     def fnv1(self, key):
         """
@@ -56,29 +52,23 @@ class HashTable:
         Implement this, and/or DJB2.
         """
 
-        FNV_OFFSET = 0xcbf29ce484222325
-        FNV_PRIME = 0x00000100000001B3
-        encoded_key = key.encode()
-
-        index = FNV_OFFSET
-        for val in encoded_key:
-            index = index * FNV_PRIME
-            index = index ^ val
-
-        return index & MAX_64_BITS
+        FNV_offset_basis = 14695981039346656037
+        bytes_representation = key.encode()
+        for byte_of_data in bytes_representation:
+            FNV_prime = 1099511628211
+            FNV_offset_basis = FNV_offset_basis * FNV_prime
+            FNV_offset_basis = FNV_offset_basis ^ byte_of_data
+        
+        hashValue = FNV_offset_basis
+        return hashValue
 
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
         Implement this, and/or FNV-1.
         """
-        encoded_key = key.encode()
-        index = 5381
-        for val in encoded_key:
-            index = ((index << 5) + index) + val
-            # index * 33 + val #
+        # Your code here
 
-        return index & MAX_32_BITS
 
     def hash_index(self, key):
         """
@@ -86,7 +76,6 @@ class HashTable:
         between within the storage capacity of the hash table.
         """
         return self.fnv1(key) % self.capacity
-        # return self.djb2(key) % self.capacity
 
     def put(self, key, value):
         """
@@ -94,53 +83,69 @@ class HashTable:
         Hash collisions should be handled with Linked List Chaining.
         Implement this.
         """
-        # TODO Check Load Factor and resize if necessary
-
-        index = self.hash_index(key)
-        cur_entry = self._get_entry(key)
-
-        # Insert the new item #
-        if cur_entry is None and self.storage[index] is not None:
-            new_entry = HashTableEntry(key, value)
-            new_entry.next = self.storage[index]
-            self._insert(new_entry, index)
-        else:
-            self._insert(HashTableEntry(key, value), index)
-
-    def _insert(self, entry, index):
-        self.storage[index] = entry
-        self.size += 1
-
+        
+        indexForKey = self.hash_index(key)
+        
+        if self.table[indexForKey] is None: #no pairs found
+            self.table[indexForKey] = HashTableEntry(key, value) #create new
+        else: #pairs found
+            pair = self.table[indexForKey]
+            while pair is not None:
+                if pair.key == key: #if exists
+                    pair.value = value #update value
+                    return
+                if pair.next is None: #last
+                    pair.next = HashTableEntry(key, value) #append new
+                    return
+                pair = pair.next #advance
+        
     def delete(self, key):
         """
         Remove the value stored with the given key.
         Print a warning if the key is not found.
         Implement this.
         """
-        # TODO Check Load Factor and resize if necessary
-
-        index = self.hash_index(key)
-        head_entry = self.storage[index]
-
-        if head_entry is None:
-            print(f"{key} is not in the table")
-        else:
-            head_entry = self._delete(key, head_entry)
-            self.storage[index] = head_entry
-
-    def _delete(self, target, cur_entry):
-        if cur_entry == None:
-            print(f"{target} is not in the table")
-            return None
-
-        if target == cur_entry.key:
-            next_entry = cur_entry.next
-            cur_entry = None
-            self.size -= 1
-            return next_entry
-
-        cur_entry.next = self._delete(target, cur_entry.next)
-        return cur_entry
+        
+        indexForKey = self.hash_index(key)
+        firstPair = True 
+        pair = self.table[indexForKey]
+        prevPair = None
+        
+        while pair is not None: #is there
+            if pair.key == key: #found it
+                if firstPair: #is head
+                    if pair.next == None: #one pair
+                        self.table[indexForKey] = None
+                        pair = None
+                    else: 
+                        self.table[indexForKey] = pair.next #shift / cut out node from list
+                        pair = None
+                else: #inside linked list
+                    if pair.next == None: #is last
+                        prevPair.next = None
+                        pair = None
+                    else: 
+                        prevPair.next = pair.next #shift / cut out node from list
+                        pair = None
+                return
+            
+            firstPair = False
+            if pair.next == None:
+                return 
+            prevPair = pair
+            pair = pair.next 
+        
+        
+        
+        
+        
+        # solution for list
+#        if self.table[indexForKey] is None: #guard, has value
+#            return
+#        for index in range (0, len(self.table[indexForKey])):
+#            if self.table[indexForKey][index].key == key:
+#                self.table[indexForKey].pop(index)
+#                return
 
     def get(self, key):
         """
@@ -148,20 +153,15 @@ class HashTable:
         Returns None if the key is not found.
         Implement this.
         """
-        entry = self._get_entry(key)
-        return entry.value if entry is not None else None
+        indexForKey = self.hash_index(key)
+        pair = self.table[indexForKey]
+        
+        while pair is not None: #there is something
+            if pair.key == key: #found it?
+                return pair.value #return it
+            pair = pair.next  #advance
 
-    def _get_entry(self, key):
-        index = self.hash_index(key)
-        cur_entry = self.storage[index]
-
-        while cur_entry is not None:
-            if cur_entry.key == key:
-                return cur_entry
-            else:
-                cur_entry = cur_entry.next
-
-        return cur_entry
+        return None
 
     def resize(self, new_capacity):
         """
@@ -169,9 +169,17 @@ class HashTable:
         rehashes all key/value pairs.
         Implement this.
         """
-        # Your code here
-        pass
+        old_table = self.table
+        old_capacity = self.capacity
 
+        self.table = [None] * new_capacity
+        self.capacity = new_capacity
+
+        for index in range(old_capacity):
+            pair = old_table[index]
+            while pair is not None:
+                self.put(pair.key, pair.value)
+                pair = pair.next 
 
 if __name__ == "__main__":
     ht = HashTable(8)
@@ -189,27 +197,21 @@ if __name__ == "__main__":
     ht.put("line_11", "So rested he by the Tumtum tree")
     ht.put("line_12", "And stood awhile in thought.")
 
-    print(ht.get("line_2"))
-    print(ht.get("line_10"))
-    print(ht.get("line_42"))
+    print("")
 
-    ht.delete("line_2")
-    print(ht.get("line_2"))
-    ht.delete("line_2")
+    # Test storing beyond capacity
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
 
-    # # Test storing beyond capacity
-    # for i in range(1, 13):
-    #     print(ht.get(f"line_{i}"))
+    # Test resizing
+    old_capacity = ht.get_num_slots()
+    ht.resize(ht.capacity * 2)
+    new_capacity = ht.get_num_slots()
 
-    # # Test resizing
-    # old_capacity = ht.get_num_slots()
-    # ht.resize(ht.capacity * 2)
-    # new_capacity = ht.get_num_slots()
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    # Test if data intact after resizing
+    for i in range(1, 13):
+        print(ht.get(f"line_{i}"))
 
-    # # Test if data intact after resizing
-    # for i in range(1, 13):
-    #     print(ht.get(f"line_{i}"))
-
-    # print("")
+    print("")
